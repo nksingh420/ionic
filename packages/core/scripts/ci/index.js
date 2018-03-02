213d83f04e8abdf0fed1cac38ad31b297ed2257f
perry <perry@ionic.io>
2018-03-02 16:56:43 -0600

    copy over dist, generate nav, and process `.md` files

--- packages/core/scripts/ci/index.js
+++ packages/core/scripts/ci/index.js
@@ -5,98 +5,118 @@ const path = require('path');
 const glob = require('glob');
 
 const config = require('./config');
+const utils = require('./utils');
+
+const ionicAngularPackage = require('../../../angular/package.json');
+const version = ionicAngularPackage.version;
+
 const startTime = new Date().getTime();
 
 async function run() {
-  vlog('Starting CI task')
-  if(!preCheck()) {
+  utils.vlog('Starting CI task')
+  if(!utils.preCheck()) {
     console.error(
       `CI Precheck Failure.
 Check configs, and that the ionic-team/ionic-docs repo is cloned`
     );
     return;
   } else {
-    vlog('Precheck complete')
+    utils.vlog('Precheck complete')
   }
-  let files = await promisedGlob(`${config.PATH_SRC}/**/readme.md`);
-  // let files = fileTypeFinder(path.join(__dirname, config.PATH_SRC),'.md');
-  copyFiles(files, `${config.PATH_DOCS}/src/docs-content`).then(()=> {
 
-    const endTime = new Date().getTime();
-    console.log(`Docs parsed and copied in ${endTime - startTime}ms`)
-  });
+  let files = await utils.promisedGlob(`${config.PATH_SRC}/**/readme.md`);
+  const DOCS_DEST = path.join(
+    config.PATH_DOCS,
+    '/src/docs-content/api/',
+    version
+  );
+
+  generateNav(
+    path.join(config.PATH_DOCS, '/src/components/site-menu/api-menu.json'),
+    files,
+    version
+  )
+
+  // parse and copy markdown and demo files
+  await copyFiles(files, DOCS_DEST);
+
+  copyDist();
+
+  const endTime = new Date().getTime();
+  console.log(`Docs copied in ${endTime - startTime}ms`);
 }
 
-function promisedGlob(pattern, options) {
-  return new Promise((resolve, reject) => {
-    glob(pattern, options, (err, files) => {
-      if(err) {
-        return reject(err);
+function copyDist() {
+  utils.copyFileSync(
+    path.join(config.PATH_SRC, '../dist/ionic.js'),
+    path.join(config.PATH_DOCS, '/src/docs-content/api/',
+      version,
+      'dist/ionic.js'
+    )
+  )
+
+  utils.copyDirectory(
+    path.join(config.PATH_SRC, '../dist/ionic'),
+    path.join(
+      config.PATH_DOCS,
+      '/src/docs-content/api/',
+      version,
+      'dist'
+    )
+  )
+}
+
+// upsert the current version's components to the nav
+function generateNav(menuPath, files, version) {
+  let menu = JSON.parse(fs.readFileSync(menuPath));
+
+  let components = {};
+  for (var i = 0; i < files.length; i++) {
+    const componentName = utils.filterParentDirectory(files[i]);
+    components[componentName] = `/docs/api/${version}/${componentName}`;
+  }
+
+  menu[version] = components;
+  fs.writeFileSync(menuPath, JSON.stringify(menu, null, '  '));
+}
+
+function copyFiles(files, dest) {
+  utils.vlog(`Copying ${files.length} files`);
+  let hasDemo = false;
+
+  for (var i = 0; i < files.length; i++) {
+    const componentName = utils.filterParentDirectory(files[i]);
+    let markdownName = `${componentName}.md`;
+    let demoName = `${componentName}.html`;
+
+    // copy demo if it exists and update the ionic path
+    hasDemo = utils.copyFileSync(
+      path.join(files[i].replace('/readme.md',''), 'test/standalone/index.html'),
+      path.join(dest, demoName),
+      file => {
+        return file.replace(
+          '/dist/ionic.js',
+          './dist/ionic.js'
+        );
       }
-      resolve(files);
-    })
-  })
+    );
 
-}
-
-function preCheck() {
-  return validateNodeVersion(process.version) &&
-    fs.existsSync(path.join(__dirname, config.PATH_SRC)) &&
-    fs.existsSync(path.join(__dirname, config.PATH_DOCS));
-}
-
-async function copyFiles(files, dest) {
-  vlog(`Copying ${files.length} files`)
-  for(var i = 0; i < files.length; i++) {
-    let pathArray = files[i].split('/');
-    let name = `${pathArray[pathArray.length - 2]}.md`;
-    vlog('Copying file: ', name);
-    await copyFile(files[i], path.join(`${dest}/${name}`));
-  }
-}
-
-function copyFile(source, target) {
-  var rd = fs.createReadStream(source);
-  var wr = fs.createWriteStream(target);
-  return new Promise(function(resolve, reject) {
-    rd.on('error', reject);
-    wr.on('error', reject);
-    wr.on('finish', resolve);
-    rd.pipe(wr);
-  }).catch(function(error) {
-    rd.destroy();
-    wr.end();
-    throw error;
-  });
-}
-
-// logging function that checks to see if VERBOSE mode is on
-function vlog(message, data) {
-  if(config.VERBOSE) {
-    if(data) {
-      console.log(message, data)
-    } else {
-      console.log(message)
-    }
-  }
-}
-
-function parseSemver(str) {
-  return /(\d+)\.(\d+)\.(\d+)/
-    .exec(str)
-    .slice(1)
-    .map(Number);
-}
-
-function validateNodeVersion(version) {
-  const [major, minor] = parseSemver(version);
-
-  if (major < 7 || (major === 7 && minor < 6)) {
-    throw new Error(
-      'Running the CI scripts requires Node version 7.6 or higher.'
+    // copying component markdown
+    utils.vlog('Copying file: ', markdownName);
+    utils.copyFileSync(
+      files[i],
+      path.join(dest, markdownName),
+      file => {
+        let header = '---';
+        if (hasDemo) {
+          header += "\r\n" + 'demo_url: \'/docs/docs-content/api/';
+          header += `${version}/${demoName}'`;
+        }
+        header += "\r\n" + '---' + "\r\n\r\n";
+        return header + file;
+      }
     );
   }
-  return true;
 }
 
 // Invoke run() only if executed directly i.e. `node ./scripts/e2e`
--- packages/core/scripts/ci/utils.js
+++ packages/core/scripts/ci/utils.js
@@ -6,6 +6,77 @@ const glob = require('glob');
 
 const config = require('./config');
 
+function copyDirectory(source, target) {
+  var files = [];
+
+  //check if folder needs to be created or integrated
+  var targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  //copy
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    for (let i = 0; i < files.length; i++) {
+      var curSource = path.join(source, files[i]);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyDirectory(curSource, targetFolder);
+      } else {
+        // async
+        copyFile(curSource, path.join(targetFolder, files[i]));
+      }
+    };
+  }
+}
+
+function copyFileSync(source, dest, hook) {
+  if (!fs.existsSync(source)){
+    vlog('Cannot copy - File does not exist ', source);
+    return false;
+  }
+  vlog('Copying file syncronously ', source);
+  let fileData = fs.readFileSync(source);
+
+  if (typeof hook === 'function'){
+    // note, changing fileData from buffer to string
+    // writeFileSync accepts both
+    fileData = hook(fileData.toString('utf8'));
+  }
+
+  // create parent directory if it doesn't exist
+  let pathArray = dest.split('/');
+  pathArray.pop();
+  const parentDirectory = pathArray.join('/');
+  // console.log(dest, parentDirectory)
+  if (!fs.existsSync(parentDirectory)){
+    fs.mkdirSync(parentDirectory);
+  }
+
+  fs.writeFileSync(dest, fileData);
+  return true;
+}
+
+function copyFile(source, target) {
+  var rd = fs.createReadStream(source);
+  var wr = fs.createWriteStream(target);
+  return new Promise(function(resolve, reject) {
+    rd.on('error', reject);
+    wr.on('error', reject);
+    wr.on('finish', resolve);
+    rd.pipe(wr);
+  }).catch(function(error) {
+    rd.destroy();
+    wr.end();
+    throw error;
+  });
+}
+
+function filterParentDirectory(path) {
+  let pathArray = path.split('/');
+  return pathArray[pathArray.length - 2];
+}
+
 function promisedGlob(pattern, options) {
   return new Promise((resolve, reject) => {
     glob(pattern, options, (err, files) => {
@@ -20,7 +91,7 @@ function promisedGlob(pattern, options) {
 function preCheck() {
   return validateNodeVersion(process.version) &&
     fs.existsSync(path.join(__dirname, config.PATH_SRC)) &&
-    fs.existsSync(path.join(__dirname, config.PATH_DOCS_FROM_CI));
+    fs.existsSync(path.join(__dirname, config.PATH_DOCS));
 }
 
 // logging function that checks to see if VERBOSE mode is on
@@ -53,6 +124,10 @@ function validateNodeVersion(version) {
 }
 
 module.exports = {
+  copyDirectory,
+  copyFile,
+  copyFileSync,
+  filterParentDirectory,
   parseSemver,
   preCheck,
   promisedGlob,
